250964dfa863ccee1eaa5971c4b4b14180ef9176
934403SA <148641148+934403SA@users.noreply.github.com>
2023-10-21 21:19:32 +0530

    Add files via upload

---
+++ CHALLENGE    UNIT-1/main (5).py
@@ -0,0 +1,9 @@
+# 1.1 Implement a Recursive Function to calculate the Factorial of a given Number.
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+number=4
+res=fact_rec(number)
+print("The Factorial of {} is {}".format(number,res))
